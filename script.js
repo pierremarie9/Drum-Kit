96e8e24af29ce94472392ea164201b8714ee7f00
pierremarie9 <63357279+pierremarie9@users.noreply.github.com>
2020-09-11 14:08:04 +0200

    first version finished

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener('keydown', function(e){
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; //stop the function from running all together
+    audio.currentTime=0; //rewind to the start
+    audio.play();
+    // key.classList.remove('key');
+    key.classList.add('select');
+});
+
+function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; //skip it if it's not a transform
+    this.classList.remove("select");
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
